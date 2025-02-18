ac632e66dc53f5d2d0409d2990a1274b8b291d69
ebakoyan <ebakoyan00@gmail.com>
2025-02-18 21:43:35 +0400

    increase default balance

--- src/common/db/seeders/1-insert-demo-user.ts
+++ src/common/db/seeders/1-insert-demo-user.ts
@@ -3,7 +3,7 @@ import { QueryInterface } from 'sequelize';
 const seed = () => ({
   up: async (queryInterface: QueryInterface) => {
     await queryInterface.sequelize.query(`
-        INSERT INTO users (id, "balanceInCents") VALUES (default, '100000')
+        INSERT INTO users (id, "balanceInCents") VALUES (default, '1000000')
     `);
   },
 });
